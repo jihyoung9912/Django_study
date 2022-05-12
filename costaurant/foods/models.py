7beefca48eaab107fe1e543eb6bbe2c418c38b4a
jihyoung9912 <jihyoung9912@gmail.com>
2022-05-12 12:33:22 +0900

    second coommit

--- costaurant/foods/models.py
+++ costaurant/foods/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# Models의 model을 상속받아 사용해야함
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    price = models.IntegerField()
+    img_path = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
--- costaurant/foods/views.py
+++ costaurant/foods/views.py
@@ -1,7 +1,21 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
-# Create your views here.
+from datetime import datetime
+from django.http import Http404
+from foods.models import Menu
 
 
 def index(request):
-    return render(request, 'foods/index.html')
+    context = dict()
+    today = datetime.today().date()
+    menus = Menu.objects.all()
+    context["date"] = today
+    context["menus"] = menus
+    return render(request, 'foods/index.html', context=context)
+
+
+def food_detail(request, pk):
+    context = dict()
+    menu = Menu.objects.get(id=pk)
+    context["menu"] = menu
+    return render(request, 'foods/detail.html', context=context)
